397421640f3899958ee8582c5cdda5d67124653a
Daria Odnostorontseva <62449609+DashaOdnostorontseva@users.noreply.github.com>
2025-11-18 01:33:42 +0300

    Add autocreate product and price in stripe

--- payment/models.py
+++ payment/models.py
@@ -5,5 +5,16 @@ class Item(models.Model):
     description = models.CharField(max_length=1000, help_text="Описание товара")
     price = models.FloatField(default=0, help_text="Цена товара")
 
-    stripe_product_id = models.CharField(max_length=255, help_text="ID продукта в stripe")
-    stripe_price_id = models.CharField(max_length=255, help_text="ID цены в stripe")
+    stripe_product_id = models.CharField(max_length=255, help_text="ID продукта в stripe", blank=True)
+    stripe_price_id = models.CharField(max_length=255, help_text="ID цены в stripe", blank=True)
+
+    def _set_stripe_product_id(self, product_id):
+        self.stripe_product_id = product_id
+        self.save(update_fields=["stripe_product_id"])
+
+    def _add_stripe_price_id(self, price_id):
+        self.stripe_price_id = price_id
+        self.save(update_fields=["stripe_price_id"])
+
+    def __str__(self):
+        return self.name
--- payment/views.py
+++ payment/views.py
@@ -8,22 +8,50 @@ from .models import Item
 
 stripe.api_key = settings.SECRET_KEY_STRIPE
 
+def create_stripe_product(item):
+    print(create_stripe_product)
+    product = stripe.Product.create(name=item.name)
+    item._set_stripe_product_id(product.id)
+
+    return product.id
+    
+def create_stripe_price(item, product_id):
+    print(create_stripe_price)
+    price = stripe.Price.create(
+        currency="rub",
+        unit_amount=int(item.price * 100),
+        product=product_id
+    )
+    item._add_stripe_price_id(price.id)
+
+    return price.id
+
+
+def get_stripe_price_id(item):
+    print(get_stripe_price_id)
+    if item.stripe_price_id:
+        return item.stripe_price_id
+    
+    if not item.stripe_product_id:
+        product_id = create_stripe_product(item)
+    else:
+        product_id = item.stripe_product_id
+    
+    price_id = create_stripe_price(item, product_id)
+    return price_id
+
+
 def buy(request, id):
     print("call buy", buy)
     if request.method == "GET":
         item = get_object_or_404(Item, id=id)
-
-        product = stripe.Product.create(name=item.name)
-
-        price = stripe.Price.create(
-            currency="rub",
-            unit_amount=item.price*100,
-            product=product.id
-        )
-
+        
+        price_id = get_stripe_price_id(item)
+        
         session = stripe.checkout.Session.create(
             success_url="https://example.com/success",
-            line_items=[{"price": price.id, "quantity": 2}],
+            cancel_url="https://example.com/cancel",
+            line_items=[{"price": price_id, "quantity": 1}],
             mode="payment",
         )
 
