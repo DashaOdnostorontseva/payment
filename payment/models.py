4f3c61a1d70b9057ec2a06be80d848f0c3fcac6c
Daria Odnostorontseva <62449609+DashaOdnostorontseva@users.noreply.github.com>
2025-11-18 01:11:40 +0300

    Add two fields to Item model

--- payment/models.py
+++ payment/models.py
@@ -4,3 +4,6 @@ class Item(models.Model):
     name = models.CharField(max_length=255, help_text="Наименование товара")
     description = models.CharField(max_length=1000, help_text="Описание товара")
     price = models.FloatField(default=0, help_text="Цена товара")
+
+    stripe_product_id = models.CharField(max_length=255, help_text="ID продукта в stripe")
+    stripe_price_id = models.CharField(max_length=255, help_text="ID цены в stripe")
