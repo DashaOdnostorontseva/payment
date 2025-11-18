6d3cac0650d68e7c970f492a321831468fad567d
Daria Odnostorontseva <62449609+DashaOdnostorontseva@users.noreply.github.com>
2025-11-18 14:26:06 +0300

    Add Order model

--- payment/models.py
+++ payment/models.py
@@ -18,3 +18,29 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, help_text="Дата и время создания заказа")
+    paid = models.BooleanField(default=False, help_text="Заказ оплачен")
+    stripe_session_id = models.CharField(max_length=255, help_text="ID сессии в stripe", blank=True)
+    total_amount = models.FloatField(default=0, help_text="Сумма заказа")
+
+    def __str__(self):
+        return "Order ID: " + str(self.id)
+    
+    @property
+    def total(self):
+        return sum(i.total for i in self.items.all())
+    
+    def _update_total_amount(self):
+        self.total_amount = self.total
+        self.save(update_fields=["total_amount"])
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, help_text="ID заказа из таблицы Order", on_delete=models.CASCADE, related_name="items")
+    item = models.ForeignKey(Item, help_text="ID товара из таблицы Item", on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField(default=1, help_text="Количество товаров") 
+
+    @property
+    def total(self):
+        return self.item.price * self.quantity
