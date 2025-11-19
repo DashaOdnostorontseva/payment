8d068d85147ad934e86b1ebee93fe2bf509b3369
Daria Odnostorontseva <62449609+DashaOdnostorontseva@users.noreply.github.com>
2025-11-19 20:00:45 +0300

    Fix urlpatterns

--- payment/urls.py
+++ payment/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path("", views.main, name="main"),
     path("success", views.success, name="success"),
     path("cancel", views.cancel, name="cancel"),
-    path("item/<int:id>/", views.item, name="item"), 
-    path("pay_item/<int:id>/", views.pay_item, name="pay_item"),
-    path("order/<int:id>/", views.order, name="order"),
-    path("pay_order/<int:id>/", views.pay_order, name="pay_order")
+    path("item/<int:id>", views.item, name="item"), 
+    path("pay_item/<int:id>", views.pay_item, name="pay_item"),
+    path("order/<int:id>", views.order, name="order"),
+    path("pay_order/<int:id>", views.pay_order, name="pay_order")
 ]
